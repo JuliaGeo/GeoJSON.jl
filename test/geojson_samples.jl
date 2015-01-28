d7591f4c62858a54d4bd73f2b6470a21445d8589
yeesian <yeesian@Yeesians-MacBook-Pro.local>
2015-01-27 20:54:22 -0500

    add tests

--- test/geojson_samples.jl
+++ test/geojson_samples.jl
@@ -40,6 +40,194 @@ e = """{"geometry": {"coordinates": [53, -4],
 
 f = """{"geometry": null, "id": 12, "properties": {"foo": "bar"}, "type": "Feature"}"""
 
+# Examples from https://github.com/Esri/geojson-utils/blob/master/tests/geojson.js
+
+multipolygon = """{
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "cartodb_id": 46,
+            "addr1": "18150 E. Pathfinder Rd.",
+            "addr2": "Rowland Heights",
+            "park": "Pathfinder Park"
+        },
+        "geometry": {
+            "type":"MultiPolygon",
+            "coordinates": [[[ [-117.913883,33.96657], [-117.907767,33.967747], [-117.912919,33.96445], [-117.913883,33.96657] ]]]
+        }
+    }]
+}"""
+
+realmultipolygon = """{
+        "type":"FeatureCollection",
+        "features":[
+        {
+            "type":"Feature",
+            "properties":{
+                "cartodb_id":46,
+                "addr1":"18150 E. Pathfinder Rd.",
+                "addr2":"Rowland Heights",
+                "park":"Pathfinder Park"
+            },
+            "geometry":{
+                "type":"MultiPolygon",
+                "coordinates": [
+                    [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
+                [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+                [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]
+                    ]
+            }
+        }
+        ]
+    }"""
+
+polyline = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a73ws67n775q",
+            "geometry" : {
+                "type" : "LineString",
+                "coordinates" : [[-89, 43], [-88, 44], [-88, 45]]
+            },
+            "properties" : {
+                "InLine_FID" : 0,
+                "SimLnFLag" : 0
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 2073,
+        "limit" : 1
+    }"""
+
+point = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a7vs0i9rnyyx",
+            "geometry" : {
+                "type" : "Point",
+                "coordinates" : [-89, 44]
+            },
+            "properties" : {
+                "fax" : "305-571-8347",
+                "phone" : "305-571-8345"
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 236,
+        "limit" : 1
+    }"""
+
+pointnull = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a7vs0i9rnyyx",
+            "geometry" : null,
+            "properties" : {
+                "fax" : "305-571-8347",
+                "phone" : "305-571-8345"
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 236,
+        "limit" : 1
+    }"""
+
+poly = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a7ws7wldxold",
+            "geometry" : {
+                "type" : "Polygon",
+                "coordinates" : [[[-89, 42], [-89, 50], [-80, 50], [-80, 42], [-89, 42]]]
+            },
+            "properties" : {
+                "DIST_NUM" : 7.0,
+                "LOCATION" : "Bustleton Ave. & Bowler St",
+                "PHONE" : "686-3070",
+                "DIST_NUMC" : "07",
+                "DIV_CODE" : "NEPD",
+                "AREA_SQMI" : 12.41643
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 25,
+        "limit" : 1
+    }"""
+
+polyhole = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a7ws7wldxold",
+            "geometry" : {
+                "type" : "Polygon",
+                "coordinates" : [[[-89, 42], [-89, 50], [-80, 50], [-80, 42], [-89, 42]], [[-87, 44], [-82, 44], [-82, 48], [-87, 48], [-87, 44]]]
+            },
+            "properties" : {
+                "DIST_NUM" : 7.0,
+                "LOCATION" : "Bustleton Ave. & Bowler St",
+                "PHONE" : "686-3070",
+                "DIST_NUMC" : "07",
+                "DIV_CODE" : "NEPD",
+                "AREA_SQMI" : 12.41643
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 25,
+        "limit" : 1
+    }"""
+
+collection = """{
+        "next_feature" : "1",
+        "type" : "FeatureCollection",
+        "start" : 0,
+        "features" : [{
+            "type" : "Feature",
+            "id" : "a7xlmuwyjioy",
+            "geometry" : {
+                "type" : "GeometryCollection",
+                "geometries" : [{
+                    "type" : "Polygon",
+                    "coordinates" : [[[-95, 43], [-95, 50], [-90, 50], [-91, 42], [-95, 43]]]
+                }, {
+                    "type" : "Polygon",
+                    "coordinates" : [[[-89, 42], [-89, 50], [-80, 50], [-80, 42], [-89, 42]]]
+                }, {
+                    "type" : "Point",
+                    "coordinates" : [-94, 46]
+                }]
+            },
+            "properties" : {
+                "STATE_ABBR" : "ZZ",
+                "STATE_NAME" : "Top"
+            }
+        }],
+        "sort" : null,
+        "page" : 0,
+        "count" : 3,
+        "limit" : 1
+    }"""
+
 # Example from osmbuildings.org/examples/GeoJSON.php
 
 osm_buildings = """{
