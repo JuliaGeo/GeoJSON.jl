dda1c262fcaeb559f27068b5a726b49c898b940f
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-12-07 16:00:29 -0500

    refactor: add omit_null to write functions for optional field handling

--- src/io.jl
+++ src/io.jl
@@ -47,12 +47,12 @@ If `geometry` is a `Tables.Table`, you may pass a `Symbol` to the `geometrycolum
 to indicate which column of the table holds the geometries.  Note that this will not keep the name,
 the geometry must be written to the `:geometry` column of the GeoJSON according to the spec.
 """
-write(io, obj::GeoJSONT) = JSON.json(io, obj)
-write(obj::GeoJSONT) = JSON.json(obj)
+write(io, obj::GeoJSONT) = JSON.json(io, obj; omit_null=true)
+write(obj::GeoJSONT) = JSON.json(obj; omit_null=true)
 
 # GeoInterface supported objects
-write(io, obj; geometrycolumn = first(GI.geometrycolumns(obj))) = JSON.json(io, _lower(obj; geometrycolumn))
-write(obj; geometrycolumn = first(GI.geometrycolumns(obj))) = JSON.json(_lower(obj; geometrycolumn))
+write(io, obj; geometrycolumn = first(GI.geometrycolumns(obj))) = JSON.json(io, _lower(obj; geometrycolumn); omit_null=true)
+write(obj; geometrycolumn = first(GI.geometrycolumns(obj))) = JSON.json(_lower(obj; geometrycolumn); omit_null=true)
 
 function _lower(obj; geometrycolumn = first(GI.geometrycolumns(obj)))
     if GI.isfeaturecollection(obj)
